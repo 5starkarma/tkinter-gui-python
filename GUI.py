bda3c2fa26cef3804a6d6f5b809e3941ce2b54af
David Alford <davidalford678@gmail.com>
2018-12-30 15:52:49 -0800

    Changed tab names to default names

--- GUI.py
+++ GUI.py
@@ -9,16 +9,16 @@ class GUI(tk.Tk):
         self.geometry('900x600')
         self.resizable(width=False, height=False)
 
-        names = ['Messenger', 'Help']
+        names = ['Tab_1', 'Tab_2']
         self.nb = self.create_notebook(names)
         self.menu = self.create_menus()
 
-        mess_tab = self.nb.tabs['Messenger']
+        mess_tab = self.nb.tabs['Tab_1']
 
-        mess_lbl_frm = ttk.LabelFrame(mess_tab, text='yo')
+        mess_lbl_frm = ttk.LabelFrame(mess_tab, text='Label Frame')
         mess_lbl_frm.grid(row=0, column=0, sticky=tk.NSEW)
 
-        mess_lbl = ttk.Label(mess_lbl_frm, text='yo')
+        mess_lbl = ttk.Label(mess_lbl_frm, text='This is a label')
         mess_lbl.grid(row=0, column=0)
 
         self.mainloop()
@@ -27,13 +27,12 @@ class GUI(tk.Tk):
         nb = MyNotebook(self, names)
         nb.grid(sticky=tk.NSEW)
 
-        help_tab = nb.tabs['Help']
-        help_lblfrm = ttk.Labelframe(help_tab, text="Help")
+        help_tab = nb.tabs['Tab_2']
+        help_lblfrm = ttk.Labelframe(help_tab, text='Tab_2')
         help_lblfrm.grid(row=0, column=0)
         help_lbl = ttk.Label(
             help_lblfrm, text='nothing here yet')
         help_lbl.grid(row=0, column=0, padx=106, pady=64)
-
         return nb
 
     def create_menus(self):
